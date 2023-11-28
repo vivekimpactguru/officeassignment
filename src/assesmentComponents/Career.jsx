1244d2b09ac64a67686b2bf41adca57dd3fd21f6
vivekimpactguru <vivek.pandey@impactguru.com>
2023-11-28 14:41:14 +0530

    ui changes

--- src/assesmentComponents/Career.jsx
+++ src/assesmentComponents/Career.jsx
@@ -1,48 +1,164 @@
-import { Carousel } from 'antd'
-import React from 'react'
+import React from "react";
+import { styled } from "@mui/material/styles";
+import {
+  Container,
+  Stack,
+  Typography,
+  Box,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
 
-function Career() {
-    const contentStyle = {
-        height: '160px',
-        color: '#fff',
-        lineHeight: '160px',
-        textAlign: 'center',
-        background: '#364d79',
-      };
+const CustomSection = styled("section")(() => ({
+  padding: "40px 0px",
+  paddingBottom: "90px",
+  backgroundColor: "#173649",
+  marginTop: "0px",
+}));
 
+const CustomRightArrow = styled("div")(() => ({
+  //   backgroundColor: "#015e7b",
+  position: "absolute",
+  padding: "5px 10px",
+  cursor: "pointer",
+  zIndex: "1",
+  border: "1px solid white",
+  borderRadius: "20%",
+  maxHeight: "40px",
+  color: "#fff",
+  top: "105%",
+  right: "4%",
+  fontSize: "2rem",
+}));
+
+const CustomLeftArrow = styled("div")(() => ({
+  //   backgroundColor: "#015e7b",
+  border: "1px solid white",
+  padding: "5px 10px",
+  position: "absolute",
+  cursor: "pointer",
+  zIndex: "1",
+  borderRadius: "20%",
+  maxHeight: "40px",
+  color: "#fff",
+  top: "105%",
+  left: "60%",
+  fontSize: "2rem",
+}));
+
+function NextArrow({ onClick }) {
   return (
-    <div className='bg-[#1F3547] md:p-10 lg:p-20' >
-
-        <div className="max-w-xs sm:max-w-sm md:max-w-4xl lg:max-w-7xl  m-auto md:flex justify-between items-center px-2 py-2 md:py-10 relative">
-           <div className="text-content max-w-[180px]  md:max-w-[450px]">
-            <h2 className="text-white font-bold text-3xl md:text-6xl mr-5">Looking For a New  Career  Opportunity? We  Can Help.</h2>
-           </div>
-           {/* <div className="bg-[url(https://i.ibb.co/XXVrhmG/image1.png)] w-full -ml-10 bg-no-repeat"> */}
-
-        <div className="image1 hidden md:block  absolute  ">
-        </div>
-        <div className='carousel max-w-[300px]'>
-           <Carousel draggable >
-                    <div   className="bg-[#E3E8ED] p-3 md:p-8     rounded-xl ">
-                        <p className='w-full  md:text-lg md:w-64 md:h-[270px] overflow-x-hidden' >“I have been with LG for 5 years and the opportunities for growth are here. You are tested every day on how badly you want success. LG and all the people here have helped me build myself and my performance. The diversity that we see every day makes us relate to people and makes our work environment stronger.”</p>
-                        <p className='text-lg font-bold' >- Lupe</p>
-                    </div>
-                    <div  className="bg-[#E3E8ED] p-3 md:p-8     rounded-xl  ">
-                        <p className='w-full  md:text-lg md:w-64 md:h-[270px] overflow-x-hidden' >“I am very grateful to LG for the opportunity it gave me. A former LG worker saw potential in me when I was working in a warehouse and offered me the opportunity to interview to work with LG as a recruiter. I started working and a few months later I was promoted to manager. I had no experience but LG helped me and gave me the necessary resources to improve my skills and grow as a person in my professional field.”</p>
-                        <p className='text-lg font-bold' >- Yizalie</p>
-                    </div>
-                    <div  className="bg-[#E3E8ED] p-3 md:p-8     rounded-xl  ">
-                        <p className='w-full  md:text-lg md:w-64 md:h-[270px] overflow-x-hidden' >“LG has improved my life drastically in the last 3 years. I started out as a temp through the agency and eventually got offered to come work in the office as a recruiter. Now I'm managing the office. The opportunity in the company is huge for whoever wants it. Working for LG has helped me improve my personal and my professional life.”</p>
-                        <p className='text-lg font-bold' >- Maynard R.</p>
-                    </div>
-             
-           </Carousel>
-        
-           {/* </div> */}
-           </div>
-        </div>
-    </div>
-  )
+    <CustomRightArrow onClick={onClick}>
+      <ArrowForwardIosIcon />
+    </CustomRightArrow>
+  );
 }
 
-export default Career
+function PrevArrow({ onClick }) {
+  return (
+    <CustomLeftArrow onClick={onClick}>
+      <ArrowBackIosNewIcon />
+    </CustomLeftArrow>
+  );
+}
+
+function Career() {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
+  const setting = {
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+  };
+
+  const data = [
+    {
+      id: 1,
+      title:
+        "The staff at LG Resources in Camp Hill, Pennsylvania were very patient and helpful with the application process. And the first site they sent me to the Employers hired me the very next day after I toured their facility. Thanks to you all at LG.I am forever grateful for the opportunity. Anyone seeking employment, I would recommend you go to LG RESOURCES STAFFING AGENCY. THANK YOU AND GOD BLESS",
+    },
+    {
+      id: 2,
+      title:
+        "The staff at LG Resources in Camp Hill, Pennsylvania were very patient and helpful with the application process. And the first site they sent me to the Employers hired me the very next day after I toured their facility. Thanks to you all at LG.I am forever grateful for the opportunity. Anyone seeking employment, I would recommend you go to LG RESOURCES STAFFING AGENCY. THANK YOU AND GOD BLESS",
+    },
+    {
+      id: 3,
+      title:
+        "The staff at LG Resources in Camp Hill, Pennsylvania were very patient and helpful with the application process. And the first site they sent me to the Employers hired me the very next day after I toured their facility. Thanks to you all at LG.I am forever grateful for the opportunity. Anyone seeking employment, I would recommend you go to LG RESOURCES STAFFING AGENCY. THANK YOU AND GOD BLESS",
+    },
+  ];
+
+  return (
+    <CustomSection>
+      <Container>
+        <Stack direction={{ xs: "column", sm: "column", lg: "row" }}>
+          <Typography
+            variant={isMobile ? "h4" : "h3"}
+            sx={{
+              textAlign: isMobile && "center",
+              color: "#fff",
+              margin: "auto",
+            }}
+          >
+            Looking For a New Career Opportunity? We Can Help.
+          </Typography>
+          {!isMobile && (
+            <Box
+              sx={{
+                width: "800px",
+                position: "relative",
+                left: "12%",
+                backgroundImage:
+                  "url(https://jobs.lgresources.com/hubfs/Group-26730.png)",
+                backgroundSize: "contain",
+                backgroundPosition: "center center",
+                backgroundRepeat: "no-repeat",
+                padding: "10px",
+                borderRadius: "20px",
+              }}
+            />
+          )}
+
+          <Box
+            sx={{
+              width: "320px",
+            }}
+          >
+            <Slider
+              {...setting}
+              slidesToShow={1}
+              slidesToScroll={1}
+              autoplay
+              autoplaySpeed="1000"
+            >
+              {data.map((value) => (
+                <Box key={value.id}>
+                  <Box
+                    sx={{
+                      //   border: "1px solid black",
+                      backgroundColor: "#fff",
+                      color: "black",
+                      width: "280px",
+                      padding: "10px",
+                      borderRadius: "20px",
+                    }}
+                  >
+                    <Typography>{value.title}</Typography>
+                  </Box>
+                </Box>
+              ))}
+            </Slider>
+          </Box>
+        </Stack>
+      </Container>
+    </CustomSection>
+  );
+}
+
+export default Career;
--- src/assesmentComponents/Footer.jsx
+++ src/assesmentComponents/Footer.jsx
@@ -1,55 +1,118 @@
-import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from "react";
+import { styled } from "@mui/material/styles";
+import {
+  Box,
+  Container,
+  Divider,
+  Grid,
+  Stack,
+  Typography,
+  useTheme,
+  useMediaQuery,
+} from "@mui/material";
+import FacebookIcon from "@mui/icons-material/Facebook";
+import YouTubeIcon from "@mui/icons-material/YouTube";
 
+const CustomSection = styled("section")(() => ({
+  padding: "50px",
+  backgroundColor: "#1f3547",
+}));
 
-
-const Footer = () => {
+function Footer() {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
   return (
-    <div className='Footer bg-slate-800'>
-        <div className='Footer_main bg-slate-800 flex px-16 pt-16 pb-20'>
-        
-            <div>
-                <div>
-                    <img src="https://jobs.lgresources.com/hubfs/files_2023/logo-bottom.svg" alt="" />
-                </div>
-                <div className='bg-orange-600'>
-                    <div className='bg-yellow-400 font-bold text-2xl w-10 h-10'>
-                        <FontAwesomeIcon icon="fa-brands fa-facebook-f" style={{color: "#FFF", fontSize:"rem"}}/>
-                    </div>
-                    <div>
-
-                    </div>
-                </div>
-            </div>
-
-            <div className='flex flex-col ml-28'>
-                <h1 className='font-family text-lg font-semibold text-white'> SALT LAKE </h1>
-                <div className='border border-t-0 border-r-0 border-b-1 border-l-0 w-6 mt-1'></div>
-                <p className='font-family text-sm font-normal text-white py-8'> 2250 S Redwood Rd. Ste 1 <br /> West Valley, UT 84119 <br /> 801-973-6755 </p>
-                <h1 className='font-family text-lg font-semibold text-white'> ROY OFFICE </h1>
-                <div className='border border-t-0 border-r-0 border-b-1 border-l-0 w-6 mt-1'></div>
-                <p className='font-family text-sm font-normal text-white pt-8'> 1952 W. 5600 S. Ste. 4 <br /> Roy Utah 84067 <br /> 801-399-4473 </p>
-            </div>
-
-            <div className='ml-52'>
-                <h1 className='font-family text-lg font-semibold text-white'> PROVO OFFICE </h1>
-                <div className='border border-t-0 border-r-0 border-b-1 border-l-0 w-6 mt-1'></div>
-                <p className='font-family text-sm font-normal text-white pt-8'> 363 N. University Ave, Suite 101 <br /> Provo, Utah 84601 <br /> 801-900-4726 </p>
-            </div>
-
-            <div className='ml-28'>
-                <p className='font-family text-sm font-normal text-white'> <a href=""> About Us </a> </p>
-                <p className='font-family text-sm font-normal text-white py-8'> <a href=""> LG Gives Back </a> </p>
-                <p className='font-family text-sm font-normal text-white '> <a href=""> Privacy Policy </a> </p>
-                <p className='font-family text-sm font-normal text-white pt-8'> <a href=""> Contact Us </a> </p>
-            </div>
-
-
-        </div>
-        <div className='border-gray-500 border border-t-5 border-r-0 border-b-0 border-l-0 mx-20'></div>
-        <p className='font-family text-sm font-normal text-white text-center py-10'> © Copyright 2023 | Logistics Giving Resources, All Rights Reserved. | Utah Jobs | Utah Staffing | Utah Employment </p>
-    </div>
-  )
+    <CustomSection>
+      <Container>
+        <Grid container spacing={4}>
+          <Grid item lg={3}>
+            <Stack spacing={10} alignItems={isMobile && "center"}>
+              <img
+                src="https://jobs.lgresources.com/hubfs/logo.svg"
+                alt="logo"
+              />
+              <Stack direction="row">
+                <Box
+                  sx={{
+                    padding: "10px",
+                    border: "1px solid #fff",
+                    borderRadius: "10px",
+                    marginRight: "10px",
+                  }}
+                >
+                  <FacebookIcon sx={{ color: "#fff" }} />
+                </Box>
+                <Box
+                  sx={{
+                    padding: "10px",
+                    border: "1px solid #fff",
+                    borderRadius: "10px",
+                  }}
+                >
+                  <YouTubeIcon sx={{ color: "#fff" }} />
+                </Box>
+              </Stack>
+            </Stack>
+          </Grid>
+          <Grid item lg={3}>
+            <Stack>
+              <Box>
+                <Typography
+                  variant="h6"
+                  sx={{ marginBottom: "20px", color: "#fff" }}
+                >
+                  SALT LAKE
+                </Typography>
+                <Typography sx={{ marginBottom: "20px", color: "#fff" }}>
+                  2250 S Redwood Rd. Ste 1 West Valley, UT 84119 801-973-6755
+                </Typography>
+              </Box>
+              <Box>
+                <Typography
+                  variant="h6"
+                  sx={{ marginBottom: "20px", color: "#fff" }}
+                >
+                  ROY OFFICE
+                </Typography>
+                <Typography sx={{ color: "#fff" }}>
+                  1952 W. 5600 S. Ste. 4 Roy Utah 84067 801-399-4473
+                </Typography>
+              </Box>
+            </Stack>
+          </Grid>
+          <Grid item lg={3}>
+            <Box>
+              <Typography
+                variant="h6"
+                sx={{ marginBottom: "20px", color: "#fff" }}
+              >
+                PROVO OFFICE
+              </Typography>
+              <Typography sx={{ color: "#fff" }}>
+                363 N. University Ave, Suite 101 Provo, Utah 84601 801-900-4726
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid item lg={3}>
+            <Stack alignItems="center" spacing={2}>
+              <Typography sx={{ color: "#fff" }}>About Us</Typography>
+              <Typography sx={{ color: "#fff" }}>LG Gives Back</Typography>
+              <Typography sx={{ color: "#fff" }}>Privacy Policy</Typography>
+              <Typography sx={{ color: "#fff" }}>Contact Us</Typography>
+            </Stack>
+          </Grid>
+        </Grid>
+        <Divider sx={{ borderColor: "#fff", marginTop: "40px" }} />
+        <Typography
+          variant="h6"
+          sx={{ color: "#fff", marginTop: "30px", textAlign: "center" }}
+        >
+          © Copyright 2023 | Logistics Giving Resources, All Rights Reserved. |
+          Utah Jobs | Utah Staffing | Utah Employment
+        </Typography>
+      </Container>
+    </CustomSection>
+  );
 }
 
-export default Footer
+export default Footer;
